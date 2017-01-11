081354bd280b0bd0303a43c72a2d0666075d6b55
Michael Schennum <mikeschenphoto@gmail.com>
2017-01-11 15:27:31 -0700

    Revert "reverts"
    This reverts commit 178af288f66ced2d59f67a483673107f8d324845.

--- src/server/client/containers/meow-message.js
+++
@@ -1,8 +0,0 @@
-import { connect } from 'react-redux';
-import Message from '../components/message';
-
-const mapStateToProps = state => ({
-  message: state.getIn(['cat', 'hasMeowed']) ? 'The cat meowed' : 'The cat did not meow',
-});
-
-export default connect(mapStateToProps)(Message);
